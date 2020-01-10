31d89438788181ff2b114ff33f4d76d0d5333e9e
Chloe Stabinsky <chloestabinsky@gmail.com>
2020-01-10 15:06:09 -0800

    Add popup menu for location adding.

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -69,6 +71,66 @@ public class Main extends Application {
 
     private void createNewLocation() {
         System.out.println("Main.createNewLocation");
+
+        TextField locationName = new TextField();
+        TextField capitalName = new TextField();
+        TextField population = new TextField();
+        TextField size = new TextField();
+        TextField language = new TextField();
+
+        capitalName.setDisable(true);
+        population.setDisable(true);
+        size.setDisable(true);
+        language.setDisable(true);
+
+        ChoiceBox<String> measurementType = new ChoiceBox<>();
+        measurementType.getItems().addAll("Metric", "Imperial");
+
+        CheckBox isCountryCheck = new CheckBox("Is this location a livable place?");
+
+        Button locationSubmit = new Button("Add New Location");
+        locationSubmit.setDisable(true);
+
+        GridPane gridPane = new GridPane();
+        gridPane.addColumn(0, new Label("Name: "), new Label(""), new Label("Capital: "), new Label("Population: "),
+            new Label(
+                "Size: "), new Label("Language"));
+        gridPane.addColumn(1, locationName, isCountryCheck, capitalName, population, size, language);
+
+        locationName.textProperty().addListener((s,n,o) -> {
+            if(locationName.getText().isEmpty() || locationName.getText().isBlank()) {
+                locationSubmit.setDisable(true);
+            } else {
+                locationSubmit.setDisable(false);
+            }
+        });
+
+        isCountryCheck.selectedProperty().addListener((s, o, n) -> {
+            capitalName.setDisable(!n);
+            population.setDisable(!n);
+            size.setDisable(!n);
+            language.setDisable(!n);
+        });
+
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+
+        Label locationTitle = new Label("Add a New Location");
+        locationTitle.setStyle("-fx-font-size: 20px;-fx-font-weight:bold");
+        VBox locationRoot = new VBox(10, locationTitle, gridPane, locationSubmit);
+
+        locationRoot.setAlignment(Pos.CENTER);
+        locationRoot.setPadding(new Insets(20));
+
+
+        Scene locationScene = new Scene(locationRoot, 500, 500);
+        Stage newStage = new Stage();
+        newStage.setScene(locationScene);
+
+        newStage.show();
+
     }
 
     private void createNewSpecies() {
